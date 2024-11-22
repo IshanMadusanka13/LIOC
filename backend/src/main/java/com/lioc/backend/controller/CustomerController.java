197876d1eb38cf23eaee0de75ce062869e92fbb7
Ishan Madusanka <madusankaishan13@gmail.com>
2024-11-22 18:52:10 +0530

    Complete Basic CRUD

---
+++ backend/src/main/java/com/lioc/backend/controller/CustomerController.java
@@ -0,0 +1,47 @@
+package com.lioc.backend.controller;
+
+import com.lioc.backend.model.Customer;
+import com.lioc.backend.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/customer")
+@CrossOrigin
+public class CustomerController {
+
+    private CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<Customer>> getAll() {
+        return ResponseEntity.ok(customerService.getAll());
+    }
+
+    @GetMapping(value = "/search/{nic}")
+    public ResponseEntity<Customer> getByNic(@PathVariable String nic) {
+        return ResponseEntity.ok(customerService.searchCustomer(nic));
+    }
+
+    @PostMapping(value = "/save")
+    public ResponseEntity<String> addCustomer(@RequestBody Customer customer) {
+        return ResponseEntity.ok(customerService.addCustomer(customer));
+    }
+
+    @PutMapping(value = "/update/{nic}")
+    public ResponseEntity<String> updateCustomer(@RequestBody Customer customer, @PathVariable String nic) {
+        return ResponseEntity.ok(customerService.updateCustomer(customer, nic));
+    }
+
+    @DeleteMapping(value = "/delete/{nic}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable String nic) {
+        return ResponseEntity.ok(customerService.deleteCustomer(nic));
+    }
+}
