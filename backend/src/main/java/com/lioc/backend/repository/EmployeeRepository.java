95719251d7487070996c552b3d9c45036e4800fe
Ishan Madusanka <128564420+IT22540744@users.noreply.github.com>
2024-11-25 13:23:51 +0530

    Added Employees

--- backend/src/main/java/com/lioc/backend/repository/EmployeeRepository.java
+++ backend/src/main/java/com/lioc/backend/repository/EmployeeRepository.java
@@ -4,4 +4,5 @@ import com.lioc.backend.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+    Employee findByUser_UserId(int userId);
 }
